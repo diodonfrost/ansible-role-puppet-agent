43a057015b8daf56559753146f88f6015aa348c7
diodonfrost <diodon.frost@diodonfrost.me>
2018-10-25 17:31:43 +0200

    add puppet-agent install from puppetlabs repos and os repository

--- tests/inspec/install_spec.rb
+++ tests/inspec/install_spec.rb
@@ -1,10 +1,12 @@
 # Puppet install
 
+puppet_package = 'puppet-agent'
+
 control 'install-01' do
   impact 1.0
   title 'Puppet-agent package'
   desc 'Puppet-agent should be installed'
-  describe package('puppet-agent') do
+  describe package(puppet_package) do
     it { should be_installed }
   end
 end
