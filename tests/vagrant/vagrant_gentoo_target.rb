d9bc99213f7757909ee325fd4674d27777db9a9a
diodonfrost <diodon.frost@diodonfrost.me>
2020-09-08 00:51:23 +0200

    removing kitchen-ci pipeline

--- tests/vagrant/vagrant_gentoo_target.rb
+++
@@ -1,8 +0,0 @@
-Vagrant.configure(2) do |config|
-  config.vm.provision "shell", inline: <<-SHELL
-    emerge --sync
-    echo 'USE=-bindist' >> /etc/portage/make.conf
-    echo 'dev-lang/python sqlite' >> /etc/portage/package.use/python
-    emerge net-misc/openssh app-admin/sudo app-admin/ansible app-portage/gentoolkit
-  SHELL
-end
--- tests/vagrant/vagrant_openbsd_target.rb
+++
@@ -1,6 +0,0 @@
-Vagrant.configure(2) do |config|
-  config.vm.provision "shell", inline: <<-SHELL
-     pkg_add -z ansible
-     ln -s /usr/local/bin/python2.7 /usr/bin/python
-  SHELL
-end
